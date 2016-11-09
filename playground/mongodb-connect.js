47705cae8e6f38b1193565b8e6039e97a08a3f3a
rayGreycloud <rayGreycloud@gmail.com>
2016-11-09 15:11:56 -0800

    Connect to mongo database

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server');
+  }
+  console.log('Connected to Mongodb server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Darth Vader',
+    age: 45,
+    location: 'Death Star'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});

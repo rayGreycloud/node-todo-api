fad1c433f2c60cdc67a74ebcedce0954689f9434
rayGreycloud <rayGreycloud@gmail.com>
2016-11-13 12:34:29 -0800

    Add simple find method

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,25 +1,22 @@
 // const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to Mongodb server');
   }
   console.log('Connected to Mongodb server');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  db.collection('Todos').insertOne({
+    text: 'More stuff to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
   // db.collection('Users').insertOne({
   //   name: 'Anakin Skywalker',
---
+++ playground/mongodb-find.js
@@ -0,0 +1,21 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server');
+  }
+  console.log('Connected to Mongodb server');
+
+  db.collection('Todos').find().toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  // db.close();
+});

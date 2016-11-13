c0bd9fd9ef52587f9506ddea469f03f9e0578e76
rayGreycloud <rayGreycloud@gmail.com>
2016-11-13 12:46:19 -0800

    Demo method to count documents from mongodb

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -10,11 +10,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to Mongodb server');
 
-  db.collection('Todos').find({
-    _id: new ObjectID('5828ce2fdc109a0450cdb0bb')
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5828ce2fdc109a0450cdb0bb')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to fetch todos', err);
   });

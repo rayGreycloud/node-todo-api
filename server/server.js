71e3745735ca4a6ff92bc7b99f80370a631167e4
rayGreycloud <rayGreycloud@gmail.com>
2016-11-21 05:50:27 -0800

    Add user model

--- server/server.js
+++ server/server.js
@@ -5,25 +5,48 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-// Challenge: create new Todo with all parameters
-var newTodo = new Todo({
-  text: 'Fetch Mail',
-  completed: false,
-  completedAt: null
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-newTodo.save().then((doc) => {
+var newUser = new User({
+  email: 'darth@deathstar.com'
+});
+
+newUser.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unable to create user');
 });
+
+// var newTodo = new Todo({
+//   text: ' Fetch Mail ',
+//   completed: false,
+//   completedAt: 123
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });

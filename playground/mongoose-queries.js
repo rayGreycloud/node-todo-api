89a30b9a88e67014628ed16bf324581e697c8878
rayGreycloud <rayGreycloud@gmail.com>
2016-11-22 12:21:05 -0800

    Complete challenge - find user by id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,31 +3,42 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
-var id = '58349cdf8882342420f961d411';
+// Challenge
+const {User} = require('./../server/models/user');
 
+var userId = '5832fb84e6446e2c16a88170';
 
-if (!ObjectID.isValid(id)) {
-  console.log('ID not valid');
-}
-
-// returns array
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log('Todos', todos);
-});
-
-//returns object - first matching
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo', todo);
-});
-
-// returns object - auto handling of id
-Todo.findById(id).then((todo) => {
-  if (!todo) {
-    return console.log('Id not found');
+User.findById(userId).then((user) => {
+  if (!user) {
+    return console.log('User not found');
   }
-  console.log('Todo By Id', todo);
+  console.log('User By Id', user);
 }).catch((e) => console.log(e));
+
+// var id = '58349cdf8882342420f961d4';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+//
+// // returns array
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// //returns object - first matching
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// // returns object - auto handling of id
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));

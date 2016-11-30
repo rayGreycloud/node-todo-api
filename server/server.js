256a420d289343be4249fe3716af84b41a3714dc
rayGreycloud <rayGreycloud@gmail.com>
2016-11-30 14:09:37 -0800

    Add authentication middleware

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -109,18 +110,8 @@ app.post('/users', (req, res) => {
   });
 });
 
-app.get('/users/me', (req, res) => {
-  var token = req.header('x-auth');
-
-  User.findByToken(token).then((user) => {
-    if (!user) {
-      return Promise.reject();
-    }
-
-    res.send(user);
-  }).catch((e) => {
-    res.status(401).send();
-  });
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {

2c9970d01e8cf0be64831f114da570e326c775cd
rayGreycloud <rayGreycloud@gmail.com>
2016-12-03 13:53:44 -0800

    Add DELETE /users/me/token logout route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 // Model method
 UserSchema.statics.findByToken = function (token) {
   var User = this;
